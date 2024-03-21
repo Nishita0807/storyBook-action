4e4640cc9b58fb07892b26845fd09b52ebd016e7
Nishita0807 <saranj0903@gmail.com>
2024-03-21 08:08:01 +0530

    final changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Embedding Storybook's UI iframe */}
+      <iframe
+        title="Storybook"
+        src="http://localhost:6006" // Replace this URL with your Storybook's URL
+        style={{
+          width: '100%',
+          height: '100vh',
+          border: 'none',
+        }}
+      />
     </div>
   );
 }
